fabe599a7d5e3baad7da1288190cb4f5ab8f7cba
Mark Newman's Temp Account <mwntemp@Marks-MacBook-Pro.local>
2018-09-13 12:14:17 -0400

    first commit, starter code

---
+++ lab2a-promise.js
@@ -0,0 +1,30 @@
+///// Problem 1 - Fun with Promises
+
+function flip() {
+    let p = new Promise(function (resolve, reject) {
+        if (Math.random() > 0.5) resolve();
+        else reject();
+    });
+    return p;
+}
+
+//// Add code here that will "flip" the coin ten times and write the 
+//// result to the console (e.g. "Heads" or "Tails" for each flip).
+
+
+
+
+///// Problem 2 - More fun...
+
+function countBig(bignum) {
+    // Add code here that returns a Promise that will resolve after it has counted to bignum
+}
+
+
+start = Date.now();
+bignum = 1000000000;
+countBig(1000000000).then(()=> {
+    console.log("It took " + (Date.now() - start) + " ms to count to " + bignum);
+}, () => {
+    console.log("A problem occurred while trying to count to " + bignum);
+})
